e84374889cec5323d068622a7b58f715a236b5a3
eruvira <pretty.elf@mail.ru>
2022-11-21 17:16:35 +0600

    style: anumation and layout

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
+	import { reactive, ref } from 'vue';
 	import { useStore } from '@/store';
-	import VitailseLogo from '@/assets/logo.png';
 	useHead({
 		title: 'Vitailse | Opinionated vite starter template',
 	});
@@ -10,55 +10,45 @@
 </script>
 
 <template>
-	<div class="grid place-items-center min-h-[80vh]">
-		<div>
-			<img :src="VitailseLogo" width="300" class="mx-auto" />
-			<p class="lg:text-left text-center">
-				{{ t('intro.desc') }}
-				<a
-					href="https://tailwindcss.com/"
-					class="text-blue-500 hover:underline"
-					>TailwindCSS</a
-				>
-			</p>
-			<p class="text-center">
-				{{ t('intro.github') }}
-				<a
-					class="text-blue-500 hover:underline"
-					href="https://github.com/zynth17/vitailse"
-					>Github Repo</a
-				>
-			</p>
-			<div class="mt-5 text-center">
-				<button
-					@click="store.$state.count++"
-					class="
-						px-4
-						py-2
-						dark:bg-blue-800
-						bg-blue-500
-						text-white
-						rounded
-					"
-				>
-					Count : {{ store.$state.count }}
-				</button>
+	<transition name="bounce" appear>
+		<div class="p-5">
+			<h1 class="text-4xl font-medium">Test Game Center Name</h1>
+			<div class="grid justify-center mt-10 gap-2 grid-cols-3">
+				<router-link :to="{ name: 'other-page' }">
+					<div class="bg-slate-50 h-[400px] text-2xl">
+						<h4 class="text-2xl font-medium">Test Zone Name</h4>
+						<div class="mt-4">
+							<div class="border-[#9475ED] border-2 p-2 flex w-fit rounded-[10px] absolute">
+								<img src="../assets/pc_icon.svg"/>
+								<div class="text-white text-xs bg-[#9475ED] h-[1.1rem] w-[1.1rem] rounded-full flex justify-center absolute bottom-[-2px] right-[-2px]">12</div>
+							</div>
+						</div>
+					</div>
+				</router-link>
 			</div>
 		</div>
-		<router-link
-			:to="{ name: 'other-page' }"
-			class="
-				mt-5
-				text-center
-				hover:text-gray-200
-				dark:hover:text-gray-500
-				hover:underline
-			"
-			>{{ t('pages.other.menu') }}</router-link
-		>
-	</div>
+	</transition>
 </template>
 
 <route lang="yaml">
 name: home
 </route>
+
+<style scoped>
+	.bounce-enter-active {
+		animation: bounce-in 0.5s;
+	}
+	.bounce-leave-active {
+		animation: bounce-in 0.5s ease-in-out;
+		animation-direction: reverse;
+	}
+
+	@keyframes bounce-in {
+		0% {
+			transform: scale(2);
+		}
+		100% {
+			transform: scale(1);
+		}
+	}
+</style>
--- src/pages/other-page.vue
+++ src/pages/other-page.vue
@@ -1,22 +1,40 @@
 <script setup lang="ts">
+	import { reactive } from 'vue';
+	import BackIcon from '~icons/pepicons/angle-left';
 	useHead({
 		title: 'Other Pages',
+	});
+	const test = reactive({
+		count: 0,
 	});
 </script>
 
 <template>
-	<div>
-		<p>{{ $t('pages.other.desc') }}</p>
+	<div class="flex flex-col px-10">
 		<router-link
 			:to="{ name: 'home' }"
-			class="
-				mt-5
-				hover:text-gray-200
-				dark:hover:text-gray-500
-				hover:underline
-			"
-			>{{ $t('pages.home') }}</router-link
+			class="mt-5 flex items-center text-sm"
 		>
+			<BackIcon style="font-size: 1em" />
+			<p>Назад</p>
+		</router-link>
+		<transition name="slide-fade" appear>
+			<div class="text-3xl mt-8 font-bold">Test Name Zone</div>
+		</transition>
+		<transition name="bounce" appear>
+			<div class="h-[200px] bg-slate-50 mt-10">
+				<div
+					class="border-[#9475ED] border-2 p-2 flex w-fit rounded-[10px] absolute"
+				>
+					<img src="../assets/pc_icon.svg" />
+					<div
+						class="text-white text-xs bg-[#9475ED] h-[1.1rem] w-[1.1rem] rounded-full flex justify-center absolute bottom-[-2px] right-[-2px]"
+					>
+						12
+					</div>
+				</div>
+			</div>
+		</transition>
 	</div>
 </template>
 
@@ -25,3 +43,35 @@
 <route lang="yaml">
 name: other-page
 </route>
+
+<style>
+	.slide-fade-enter-active {
+		transition: all 0.3s ease-out;
+	}
+
+	.slide-fade-leave-active {
+		transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+	}
+
+	.slide-fade-enter-from,
+	.slide-fade-leave-to {
+		transform: translateX(-100px);
+		opacity: 0;
+	}
+
+	.bounce-enter-active {
+		animation: bounce-in 0.5s;
+	}
+	.bounce-leave-active {
+		animation: bounce-in 0.4s ease-in-out;
+	}
+
+	@keyframes bounce-in {
+		0% {
+			transform: scale(0);
+		}
+		100% {
+			transform: scale(1);
+		}
+	}
+</style>
